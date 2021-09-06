35b5e83cc31dfe9466e9876f31ea8cd04fa50b09
DantesHub <motiv8lab@gmail.com>
2021-09-06 13:59:16 -0400

    readd nextjs folder

---
+++ mindgarden-web/components/App.tsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+type AppProps = {
+  message: string;
+}; 
+export const App = () => <div>{"bink"}</div>;
---
+++ mindgarden-web/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import {App} from '../components/App'
+
+const Home = () => {
+  return (
+    <App/>
+  )
+}
+
+export default Home
