d216b90be750d0847b27efd0cc59b4efdf7dd97e
DantesHub <motiv8lab@gmail.com>
2021-09-16 16:49:52 -0400

    authentication - add error validation to register form

--- client/src/Authentication/RegisterBox.tsx
+++ client/src/Authentication/RegisterBox.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useForm } from 'react-hook-form'
+
+interface FormData {
+  name: string
+  email: string
+  password: string
+}
 
 export const RegisterBox = () => {
+  const [name,setName] = useState("")
+  const {register, handleSubmit,  formState: { errors }  } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(({name, email,password}) => {
+    console.log(name)
+  })
   return (
     <>
  <div className="w-full max-w-md mx-auto">
@@ -8,23 +21,44 @@ export const RegisterBox = () => {
     Register
     </div>
    </div>
-  <div className="max-w-md w-full mx-auto h-3/5  mt-4 bg-darkWhite rounded p-8 rounded neoShadow">
-    <form action="" className="space-y-5 w-full">
+  <div className="max-w-md w-full mx-auto h-4/6  mt-4 bg-darkWhite rounded p-8 rounded neoShadow">
+    <form action="" onSubmit={onSubmit} className="space-y-5 w-full">
       <div>
         <label htmlFor="" className="text-md font-bold font-mada text-gray-600 block ml-1">Name</label>
-        <input type="text" className="w-full neoShadow p-2 mt-1 bg-darkWhite mx-auto" />
+        <input  {...register("name", {required: true})} name="name" type="text" className="w-full neoShadow p-3 mt-1 bg-darkWhite mx-auto outline-none" placeholder="Sammy"  />
+        <div className="text-md font-mada text-red-600 ml-2">
+        {errors.name && "Name is invalid" }
+        </div>
         </div>
         <div>
         <label htmlFor="" className="text-md font-bold font-mada text-gray-600 block ml-1">Email</label>
-        <input type="text" className="w-full neoShadow p-2 mt-1 bg-darkWhite mx-auto" />
+        <input  {...register("email", {
+          required: true, 
+          minLength: 6, 
+          maxLength: 24,
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "invalid email address"
+          }
+          })}  type="text" className="w-full neoShadow p-3 mt-1 bg-darkWhite mx-auto outline-none" placeholder="sammy@gmail.com" name="email"/>
+        <div className="text-md font-mada text-red-600 ml-2">
+        {errors.email && "Email is invalid" }
+        </div>
         </div>
         <div>
         <label htmlFor="" className="text-md font-bold font-mada text-gray-600 block ml-1">Password</label>
-        <input type="password" className="w-full  neoShadow p-2 mt-1 bg-darkWhite mx-auto" />
+        <input  {...register("password", {
+          required: true,
+          minLength: 6, 
+          maxLength: 24,
+        })}   type="password" className="w-full  neoShadow p-3 mt-1 bg-darkWhite mx-auto outline-none" placeholder="**********" name="password" />
+        <div className="text-md font-mada text-red-600 ml-2">
+          {errors.password && "Password is invalid" }
+        </div>
         </div>
         <div>
             <div className = "neoShadow mt-12">
-          <button className="w-full py-2 px-4 hover:bg-green-500 text-white text-md bg-green-700  text-center">
+          <button className="w-full py-3 px-4 hover:bg-green-500 text-white text-md bg-green-700  text-center">
             Sign Up
           </button>
       </div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import logo from "../images/logo512.png"
 export const Navbar = () => {
   const [active, setActive] = useState(false);
-  const handleClick = () => { setActive(!active); };
+
+  const handleClick = () => { setActive(!active);};
 
   return (
     <>
@@ -16,7 +17,7 @@ export const Navbar = () => {
           </div>
         </a>
         <button
-          className=' inline-flex p-3 hover:bg-green-600 text-white ml-auto hover:text-white outline-none'
+          className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
           onClick={handleClick}
         >
           <svg
