938bfcc107095c85a091863114cb47fc86b7947e
DantesHub <motiv8lab@gmail.com>
2021-10-05 23:43:39 -0400

    add modal to store with animations

--- client/src/components/shared/DisplayCard.tsx
+++ client/src/components/shared/DisplayCard.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import redTulipPacket from "../../images/redTulipPacket.svg";
 import coin from "../../images/coin.svg";
+import { motion } from "framer-motion";
 
-interface IDisplayCard {}
+interface IDisplayCard {
+  handleOpen: () => void;
+}
 
 export const DisplayCard: React.FunctionComponent<IDisplayCard> = (props) => {
   return (
-    <div className="justify-center h-80 neoShadow items-center flex-col">
-      <img src={redTulipPacket} alt="Red Tulips Packet" className="w-7/12" />
-      <div className="font-mada font-bold text-gray-700 text-2xl w-5/6">
-        Red Tulips
-      </div>
-      <div className="flex w-full justify-center -ml-2">
-        <img src={coin} alt="coin" className="w-6 mr-2" />
-        <div className="font-mada font-semibold text-gray-700 text-2xl w-4/6">
-          200
+    <motion.button
+      onClick={props.handleOpen}
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.9 }}
+    >
+      <div className="justify-center h-80 neoShadow items-center flex-col">
+        <img src={redTulipPacket} alt="Red Tulips Packet" className="w-7/12" />
+        <div className="font-mada font-bold text-gray-700 text-2xl w-5/6">
+          Red Tulips
+        </div>
+        <div className="flex w-full justify-center -ml-2">
+          <img src={coin} alt="coin" className="w-6 mr-2" />
+          <div className="font-mada font-semibold text-gray-700 text-2xl w-4/6">
+            200
+          </div>
         </div>
       </div>
-    </div>
+    </motion.button>
   );
 };
--- client/src/pages/StorePage.tsx
+++ client/src/pages/StorePage.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { DisplayCard } from "../components/shared/DisplayCard";
 import coin from "../images/coin.svg";
+import { Modal } from "../components/shared/Modal";
 
 export interface IStorePage {}
 
 export const StorePage: React.FunctionComponent<IStorePage> = () => {
+  const [isOpen, toggle] = useState(false);
+
+  function handleOpenModal(open: boolean) {
+    console.log("close modal");
+    toggle(open);
+  }
   return (
     <div className="container h-screen mx-auto">
       <div className="flex justify-center">
@@ -16,12 +23,13 @@ export const StorePage: React.FunctionComponent<IStorePage> = () => {
         <div className="justify-center h-80 w-1/6 flex bg-red-300 neoShadow">
           1
         </div>
-        <DisplayCard />
+        <DisplayCard handleOpen={() => handleOpenModal(true)} />
         <div className="justify-center h-80 neoShadow"></div>
         <div className="justify-center h-80 flex bg-red-300 neoShadow">4</div>
         <div className="justify-center h-80 flex bg-red-300 neoShadow">5</div>
         <div className="flex justify-center">6</div>
       </div>
+      <Modal isOpen={isOpen} handleClose={() => handleOpenModal(false)} />
       <div className="flex justify-center mt-5"></div>
     </div>
   );
