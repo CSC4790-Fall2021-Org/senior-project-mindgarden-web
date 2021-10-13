72ef26aac109af5e2c94441f6186d6dae8a93c49
DantesHub <motiv8lab@gmail.com>
2021-10-12 20:40:36 -0400

    add success modal ui

--- client/src/components/shared/Modal.tsx
+++ client/src/components/shared/Modal.tsx
@@ -4,6 +4,8 @@ import { motion, AnimatePresence } from "framer-motion";
 export interface IModal {
   handleClose: () => void;
   isOpen: Boolean;
+  size: String;
+  isOkay: Boolean;
 }
 
 export const Modal: React.FunctionComponent<IModal> = (props) => {
@@ -28,42 +30,48 @@ export const Modal: React.FunctionComponent<IModal> = (props) => {
           variants={modalVariant}
         >
           <motion.div
-            className="lg:w-1/4 w-2/5 h-1/2 top-1/2 left-1/2 absolute bg-darkWhite rounded-lg transform -translate-x-1/2 -translate-y-1/2 opacity-100 flex justify-center items-center"
+            className={`${props.size} top-1/2 left-1/2 absolute bg-darkWhite rounded-lg transform -translate-x-1/2 -translate-y-1/2 opacity-100 flex justify-center items-center`}
             initial={"initial"}
             animate={"isOpen"}
             exit={"exit"}
             variants={containerVariant}
           >
-            <h1 className="">modal1</h1>
-            <svg
-              onClick={props.handleClose}
-              className="w-6 h-6 absolute left-6 top-6 cursor-pointer"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20.39 20.39"
-            >
-              <line
-                x1="19.39"
-                y1="19.39"
-                x2="1"
-                y2="1"
-                fill="none"
-                stroke="gray"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                strokeWidth="2"
-              />
-              <line
-                x1="1"
-                y1="19.39"
-                x2="19.39"
-                y2="1"
-                fill="none"
-                stroke="gray"
-                strokeLinecap="round"
-                strokeMiterlimit="10"
-                strokeWidth="2"
-              />
-            </svg>
+            <div className="flex flex-col">
+              <div className="flex">
+                {!props.isOkay && (
+                  <svg
+                    onClick={props.handleClose}
+                    className="w-6 h-6 absolute left-6 top-6 cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20.39 20.39"
+                  >
+                    <line
+                      x1="19.39"
+                      y1="19.39"
+                      x2="1"
+                      y2="1"
+                      fill="none"
+                      stroke="gray"
+                      strokeLinecap="round"
+                      strokeMiterlimit="10"
+                      strokeWidth="2"
+                    />
+                    <line
+                      x1="1"
+                      y1="19.39"
+                      x2="19.39"
+                      y2="1"
+                      fill="none"
+                      stroke="gray"
+                      strokeLinecap="round"
+                      strokeMiterlimit="10"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                )}
+              </div>
+              {props.children}
+            </div>
           </motion.div>
         </motion.div>
       )}
--- client/src/pages/StorePage.tsx
+++ client/src/pages/StorePage.tsx
@@ -1,16 +1,77 @@
 import React, { useState } from "react";
+
 import { DisplayCard } from "../components/shared/DisplayCard";
 import coin from "../images/coin.svg";
+import redTulipPacket from "../images/redTulipPacket.svg";
 import { Modal } from "../components/shared/Modal";
+import { motion } from "framer-motion";
 
 export interface IStorePage {}
 
+export interface IBuyModal {
+  toggleOkay: () => void;
+}
+export interface IOkayModal {
+  toggleOkay: () => void;
+}
+
+const BuyModal: React.FunctionComponent<IBuyModal> = (props) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="font-bold font-mada text-2xl p-4 text-center absolute xl:left-26 top-1">
+        Red Tulip
+      </h1>
+      <img src={redTulipPacket} alt="red tulip packet" className="w-40 mt-8" />
+      <div className="flex text-center font-mada text-gray-800  p-4 px-8">
+        Control the opacity of an element’s text color using the text-opacity-
+        utilities.
+      </div>
+      <div className="w-24">
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.9 }}
+          className="h-10 bg-green-600 neoShadow justify-center items-center"
+          onClick={props.toggleOkay}
+        >
+          <div className="font-mada font-bold text-white">Buy</div>
+        </motion.button>
+      </div>
+    </div>
+  );
+};
+
+const OkayModal: React.FunctionComponent<IOkayModal> = (props) => {
+  return (
+    <div className="flex flex-col justify-center items-center -mt-8">
+      <div className="flex text-center font-mada text-gray-800 p-8 font-bold">
+        Purchase successful! Check the home page, and tap plant select to change
+        your default plant.
+      </div>
+      <div className="w-1/2">
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.9 }}
+          className="h-10 bg-green-600 neoShadow justify-center items-center"
+          onClick={props.toggleOkay}
+        >
+          <div className="font-mada font-bold text-white">Okay!</div>
+        </motion.button>
+      </div>
+    </div>
+  );
+};
+
 export const StorePage: React.FunctionComponent<IStorePage> = () => {
   const [isOpen, toggle] = useState(false);
+  const [isOkayOpen, toggleOkay] = useState(false);
 
   function handleOpenModal(open: boolean) {
-    console.log("close modal");
     toggle(open);
+  }
+
+  function handleOpenOkay(close: boolean) {
+    toggle(false);
+    toggleOkay(close);
   }
   return (
     <div className="container h-screen mx-auto">
@@ -29,7 +90,22 @@ export const StorePage: React.FunctionComponent<IStorePage> = () => {
         <div className="justify-center h-80 flex bg-red-300 neoShadow">5</div>
         <div className="flex justify-center">6</div>
       </div>
-      <Modal isOpen={isOpen} handleClose={() => handleOpenModal(false)} />
+      <Modal
+        isOpen={isOpen}
+        handleClose={() => handleOpenModal(false)}
+        size={"xl:w-1/4 lg:w-2/5 w-2/5 xs:w-3/5 h-1/2"}
+        isOkay={false}
+      >
+        <BuyModal toggleOkay={() => handleOpenOkay(true)} />
+      </Modal>
+      <Modal
+        isOpen={isOkayOpen}
+        handleClose={() => toggleOkay(false)}
+        size={"h-1/4 w-1/4 xs:h-1/4 xs:w-2/3"}
+        isOkay={true}
+      >
+        <OkayModal toggleOkay={() => toggleOkay(false)} />
+      </Modal>
       <div className="flex justify-center mt-5"></div>
     </div>
   );
