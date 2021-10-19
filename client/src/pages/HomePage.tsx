62b712f1bc2530c7504379dea6b01d02199d1c75
DantesHub <motiv8lab@gmail.com>
2021-10-19 16:07:55 -0400

    add responsive drawer to home screen

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { LogoutButton } from '../components/Authentication/LogoutButton';
+import React from "react";
+import { Link } from "react-router-dom";
+import { LogoutButton } from "../components/Authentication/LogoutButton";
 
 const HomePage = () => {
   return (
-   <div>
-     home
-     <Link to="/changepassword">Change Password</Link>
-     <div>
-     <LogoutButton />
-     </div>
-   </div>
-  )
-}
+    <div className="container h-screen w-screen">
+      <div className="bg-darkWhite drawer drawer-mobile h-screen">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="flex flex-col items-center justify-center drawer-content">
+          <label
+            htmlFor="my-drawer-2"
+            className="mb-4 btn btn-primary drawer-button lg:hidden"
+          >
+            open menu
+          </label>
+          <div className="hidden text-xs text-center lg:block">
+            Menu is always open on desktop size.
+            <br />
+            Resize the browser to see toggle button on mobile size
+          </div>
+          <div className="text-xs text-center lg:hidden">
+            Menu can be toggled on mobile size.
+            <br />
+            Resize the browser to see fixed sidebar on desktop size
+          </div>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            <li>
+              <a>Menu Item</a>
+            </li>
+            <li>
+              <a>Menu Item</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
